94dea422e4aac15dad619ab1f14012a60d541740
teraxl <teraxl@akaTerk>
2018-01-02 20:12:04 +0300

    ...

--- PasswordGeneration/src/passwordgenerator/PasswordGen.java
+++ PasswordGeneration/src/passwordgenerator/PasswordGen.java
@@ -147,7 +147,7 @@ public class PasswordGen {
 
 	public static void createAndShowGUI() {
 		JFrame.setDefaultLookAndFeelDecorated(true);
-		JFrame frame = new JFrame("Генератор паролей");
+		JFrame frame = new JFrame("Password Generate");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		addComponentOnPane(frame.getContentPane());
