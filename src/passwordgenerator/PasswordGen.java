26d502c52b74562cd39f0bcf1ac78bb2ad1be6cf
СашаНастя <@192.168.0.48>
2018-01-14 01:42:24 +0300

    ...

--- src/passwordgenerator/PasswordGen.java
+++ src/passwordgenerator/PasswordGen.java
@@ -13,6 +13,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
@@ -25,7 +34,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
-import javax.swing.JSpinner;
+import javax.swing.JSpinner; 
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SpinnerModel;
@@ -155,7 +164,6 @@ public class PasswordGen {
 			public void actionPerformed(ActionEvent paramActionEvent) {
 				
 				if(txtAria.getSelectedText() == null) {
-					
 					JOptionPane.showMessageDialog(frame, "Вы не выделили ни одного символа","Внимание", JOptionPane.INFORMATION_MESSAGE);
 				} else {
 
