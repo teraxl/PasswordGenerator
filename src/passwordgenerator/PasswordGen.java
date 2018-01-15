1092f36c26de06581cb5fb5f2ab28e2b31497aae
Администратор <@pc>
2018-01-15 22:25:15 +0300

    ....

--- src/passwordgenerator/PasswordGen.java
+++ src/passwordgenerator/PasswordGen.java
@@ -31,6 +31,8 @@ import javax.swing.JTextField;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.Border;
 
 public class PasswordGen extends ClassLoader {
@@ -133,6 +135,21 @@ public class PasswordGen extends ClassLoader {
 	
 	public static void createAndShowGUI() {
 //		JFrame.setDefaultLookAndFeelDecorated(true);
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InstantiationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		JFrame frame = new JFrame("Генератор паролей");
 		ImagePack imagePack = new ImagePack();
 		//image = Toolkit.getDefaultToolkit().getImage("images/image_icon.png");
